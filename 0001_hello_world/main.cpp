bd40fe52f7bf4387fb02cd8b715d1586310ae7f8
Roberto Prince <roberto_epe@hotmail.com>
2022-07-17 00:04:01 -0500

    Added a example hello, world

---
+++ 0001_hello_world/main.cpp
@@ -0,0 +1,12 @@
+/*
+* Program that prints Hello, World! in the console
+*/
+
+#include <iostream>
+
+int main(){
+    // std::cout << "Hello, World!" << std::endl; // endl adds a new line and flush the console
+    std::cout << "Hello, World!\n"; // We use a scape character \n witch is a new line
+
+    // return 0; // Return 0 is optional in c++ standar
+}
