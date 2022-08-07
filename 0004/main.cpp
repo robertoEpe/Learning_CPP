8988c9cab938a8add5c855c4eb37ef704f6bfa88
Roberto Prince <roberto_epe@hotmail.com>
2022-08-07 06:05:14 -0500

    Add program that computes faren to celsius

--- 0004/main.cpp
+++ 0004/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    // Print Hello, world | \n is a new line
+    std::cout << "Please enter a degree value in Celsius :\n";
+    double celsius;
+    std::cin >> celsius;
+    double fahrenheit = (9.0 / 5) * celsius + 32;
+    std::cout << celsius << " Celsius is " << fahrenheit << " Farenheit\n";
+    // Finishing successfully the program
+    return EXIT_SUCCESS;
 }
