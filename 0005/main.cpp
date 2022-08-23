279760361d77fd3915a76694234857d24073d7dd
RobertoPrince <roberto_epe@hotmail.com>
2022-08-23 11:32:34 -0500

    Added simple program that prints a diamond

---
+++ 0005/main.cpp
@@ -0,0 +1,18 @@
+/*
+*   Print a diamond
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout   << "  *  \n"
+                << " *** \n"
+                << "*****\n"
+                << " *** \n"
+                << "  *  \n"
+    
+    ;
+
+    return EXIT_SUCCESS;
+}
