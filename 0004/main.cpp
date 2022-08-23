b27ba7ca4aa33c2236e85cf49449f9c478fa9a5e
RobertoPrince <roberto_epe@hotmail.com>
2022-08-23 09:54:55 -0500

    added coment

--- 0004/main.cpp
+++ 0004/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     // Print Hello, world | \n is a new line
     std::cout << "Please enter a degree value in Celsius :\n";
+    // grades in celsius as input
     double celsius;
     std::cin >> celsius;
     double fahrenheit = (9.0 / 5) * celsius + 32;
